d188bff2757d7fc0b96bcc1c4418ef26385a493b
Dragon4396 <hekunlong2515@gmail.com>
2023-11-01 06:51:38 -0700

    add Play page UI

---
+++ src/modules/player.js
@@ -0,0 +1,4 @@
+export let player = {
+    space: 0,
+    money: 1500
+};
--- src/pages/play.js
+++ src/pages/play.js
@@ -1,5 +1,13 @@
+
+import  position from "../images/position.png"
+ 
 export const Play = () => {
+    const a = 10;
     return (
-        <div> This is Play </div>
+        <div style={{position: 'relative', width: '200vh', height: '100vh', backgroundColor: 'lightblue'}}>
+            <div style={{position: 'absolute', top: '5vh', left: '10vh', width: '180vh', height: '90vh', backgroundColor: 'white'}}>
+                <img src={position} alt="position" style={{ position: 'absolute', top: `${a}%`, left: '20%', width: '20px', height: 'auto'}}/>
+            </div>
+        </div>
     );
 }
