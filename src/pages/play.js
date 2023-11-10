1e8947e79928c770e927eecc1622203f4b267310
Dragon4396 <hekunlong2515@gmail.com>
2023-11-09 21:52:11 -0800

    display the spaceInfo after rolling the dice

--- src/pages/play.js
+++ src/pages/play.js
@@ -1,14 +1,14 @@
 import { SeperatorLine } from "../modules/seperatorLine";
 import { PlayerInfo } from "../modules/playerInfo";
 import { DisplayDiceNumber } from "../modules/displayDiceNumber";
-import { Map } from "../modules/map";
+import { Space } from "../modules/space";
+import { element } from "../elements/home/element";
+import { SpaceInfo } from "../modules/spaceInfo";
 import { ReturnToHome } from "../modules/returnToHome";
 import { Surrender } from "../modules/surrender";
 import { useState, useEffect } from "react";
 import { GetRandomGoods } from "../utils/GetRandomGoods";
 import { useNavigate } from "react-router-dom";
-import { element } from "../elements/home/element";
-import { SpaceInfo } from "../modules/spaceInfo";
 
 export function Play() {
     const [number, setNumber] = useState(0);
@@ -16,6 +16,7 @@ export function Play() {
     const [position, setPosition] = useState(0);
     const [money, setMoney] = useState(1500);
     const [goods, setGoods] = useState(GetRandomGoods);
+    const [spaceInfo, setSpaceInfo] = useState("");
     const navigate = useNavigate();
 
     const handleRoll = () => {
@@ -24,9 +25,19 @@ export function Play() {
         setPosition((position + randomNum) % 36);
     }
 
-    const handleEvent = (position) => {
-        console.log(element[position]);
-    }
+    const handleClick = (element) => {
+        setSpaceInfo(<span> <p> {element.text} </p>
+        IDs: {element.ids.map((id, index)=>(id+" "))} <br />
+        Category: {element.elements} <br />   
+        {element.elements === "Properties" && <span> Cost: {element.cost} <br />
+                                                    Update: {element.update} <br />
+                                                    level: {element.level} <br />
+                                                    Sell: {element.sell} <br />
+                                                    Withdraw: {element.withdraw} <br />
+                                                    Toll: {element.toll} <br />
+                                                    Required Goods / Purchase Price: {element.required.map((good, index)=>(<li key={index}>{`${good.good} / ${good.price}`}</li>))}</span>}
+                                               </span>)
+    };
 
     useEffect(()=>{
         if(position - number < 0) {
@@ -36,7 +47,7 @@ export function Play() {
     },[position, number])
 
     useEffect(()=>{
-        handleEvent(position);
+        handleClick(element[position]);
     },[position])
 
     useEffect(()=>{
@@ -57,8 +68,21 @@ export function Play() {
             <PlayerInfo round={round} position={position} money={money} goods={goods}/>
             <button onClick={handleRoll} className="Roll"> ROLL </button>
             <DisplayDiceNumber number={number}/>
-            <Map position={position}/>
+            
+            {element.map((el, index) => (
+                <Space
+                    key={index} 
+                    id={index}
+                    top={index >= 0 && index < 11 ? 50 : (index >= 11 && index < 18 ? 50 + 50 * (index - 11) : (index >= 18 && index < 29 ? 400 : 400 - 50 * (index - 29)))}
+                    left={index >= 0 && index < 11 ? 300 + 50 * index : (index >= 11 && index < 18 ? 850 : (index >= 18 && index < 29 ? 850 - 50 * (index - 18) : 300))}
+                    text={el.text}
+                    position={position}
+                    onSpaceClick={() => handleClick(el)}
+                />
+            ))}
 
+            <SpaceInfo spaceInfo={spaceInfo}/>
+            
             <ReturnToHome />
             <Surrender />
         </div>
