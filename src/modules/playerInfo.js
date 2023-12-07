91e94464b9697c281d28d065735bc0a220d05ed4
Dragon4396 <hekunlong2515@gmail.com>
2023-12-06 23:09:36 -0800

    adjust the margin size of the playerInfo and spaceInfo

--- src/modules/playerInfo.js
+++ src/modules/playerInfo.js
@@ -1,6 +1,6 @@
 export function PlayerInfo ({round, position, money, goods}) {
     return (
-        <div style={{position: 'absolute', top: '1%', left: '1%', width: '220px', height: '180px', border: '0.05vh solid black'}}>
+        <div style={{position: 'absolute', top: '1%', left: '1%', width: '220px', height: '200px', border: '0.05vh solid black'}}>
             <div style={{position: 'absolute', left: '3%', textAlign: 'left', fontSize: '13px'}}>
                 <p> Player Info </p>
                 <span> 
--- src/modules/spaceInfo.js
+++ src/modules/spaceInfo.js
@@ -1,6 +1,6 @@
 export function SpaceInfo ({spaceInfo}) {
     return (
-        <div style={{position: 'absolute', top: '39%', left: '1%', width: '220px', height: '260px', border: '0.05vh solid black'}}>
+        <div style={{position: 'absolute', top: '43%', left: '1%', width: '220px', height: '240px', border: '0.05vh solid black'}}>
             <div style={{position: 'absolute', left: '3%', textAlign: 'left', fontSize: '13px'}}>
                 {spaceInfo}
             </div>
