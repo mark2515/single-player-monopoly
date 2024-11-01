6f04e32b3504a6851c72181b1c81a85e90f1b71a
mark2515 <hekunlong2515@gmail.com>
2024-11-01 14:55:28 -0700

    update background

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,27 @@
 import { Link } from "react-router-dom";
+import '../style.css';
 
 export function HomePage() {
     return (
-        <div className="Home">
-            <h1> Single-Player Monopoly </h1>
-            <Link to="/play" className="HomeButtons"> PLAY </Link>
-            <br />
-            <Link to="/rules" className="HomeButtons"> RULES </Link>
-            <br />
-            <Link to="/credits" className="HomeButtons"> CREDITS </Link>
-        </div>
+        <section>
+            <div className="Home">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <h1> Single-Player Monopoly </h1>
+                <Link to="/play" className="HomeButtons"> PLAY </Link>
+                <br />
+                <Link to="/rules" className="HomeButtons"> RULES </Link>
+                <br />
+                <Link to="/credits" className="HomeButtons"> CREDITS </Link>
+            </div>
+        </section>
     );
 }
