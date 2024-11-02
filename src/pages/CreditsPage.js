247639ac0f722f3625e798b0b868da276dac55f1
mark2515 <hekunlong2515@gmail.com>
2024-11-01 17:44:20 -0700

    implement confetti effect

--- src/pages/CreditsPage.js
+++ src/pages/CreditsPage.js
@@ -1,27 +1,45 @@
 import { useState } from "react";
 import { ReturnToHome } from "../modules/returnToHome";
+import confetti from 'canvas-confetti';
+import '../style.css';
 
 export function CreditsPage() {
     const [visible, setVisible] = useState(false);
     const handleClick = () => {
         setVisible(true)
+        confetti({
+            particleCount: 150,
+            spread: 60
+        });
     }
     return (
-        <div className="Outer">
-            <div style={{textAlign: 'center', fontSize: '24px'}}>
-            {!visible && (
-                <>
-                    <p style={{fontWeight: 'bold'}}>Credits</p>
-                    This game was independently developed by Mark He. If you enjoy it, please give him a thumbs up!
-                </>
-            )}
-            {visible && <p style={{fontWeight: 'bold'}}>Thank You !</p>}
-                <div style={{position: 'absolute', top: '80%', left: '50%'}}>
-                    <button onClick={handleClick}>&#128077;</button>
+        <section>
+            <div className="Home">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <div style={{textAlign: 'center', fontSize: '24px'}}>
+                {!visible && (
+                    <>
+                        <p style={{fontWeight: 'bold'}}>Credits</p>
+                        This game was independently developed by Mark He. If you enjoy it, please give him a thumbs up!
+                    </>
+                )}
+                {visible && <p style={{fontWeight: 'bold'}}>Thank You !</p>}
+                    <div style={{position: 'absolute', top: '80%', left: '49%'}}>
+                        <button onClick={handleClick}>&#128077;</button>
+                    </div>
                 </div>
-            </div>
 
                 <ReturnToHome />
-        </div>
+            </div>
+        </section>
     );
 }
